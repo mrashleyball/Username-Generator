0a39e2690c44cd6382c7bb447fca31f3cfdae710
Ashley Ball <me@ashleyball.com.au>
2021-10-15 20:52:41 +0000

    Initial commit

---
+++ username-gen-1.py
@@ -0,0 +1,16 @@
+# Username Generator Program
+
+#1 Display title and instructions
+#2 Ask for user input, first and last name
+#3 Slice four letters of last name
+#4 Slice first letter of first name
+#5 Combine sliced letters to create username
+
+print('Welcome to the Username Generator Program.')
+
+first = input('Enter first name: ')
+last = input('Enter last name: ')
+
+# Slicing done here
+
+print('Your username is: ' + last + first)
